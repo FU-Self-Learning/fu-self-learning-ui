8f34216015939fd5ec96d7579625806f032eb418
Đào Bá Anh Quân <quandba154@gmail.com>
2025-05-13 00:26:26 +0700

    Refactor LoginPage layout and update background image

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,26 +5,39 @@ import { RegisterForm } from "@/components/register/RegisterForm";
 import { Typography, Button, Divider } from "antd";
 import { useState } from "react";
 import { GoogleOutlined } from "@ant-design/icons";
+import Image from "next/image";
+import LoginBanner from "@p/images/Login.png"
 
 export default function LoginPage() {
   const [isLoginFormVisible, setIsLoginFormVisible] = useState(true);
   return (
-    <div className="min-h-[88vh] flex bg-[url('/login-bg.jpg')] bg-cover bg-center">
-      <div className="w-1/2 relative hidden lg:flex">
-        <div className="absolute inset-0 bg-black opacity-30" />
-        <div className="z-10 p-12 text-white self-center">
-          <h1 className="text-4xl font-bold mb-4">SoundClient</h1>
-          <p className="text-lg mb-1">
-            Comprehensive music management solution
-          </p>
-          <p className="italic text-sm mt-4">
-            “Stream music seamlessly – Discover new tracks – Manage your
-            playlists from start to finish.”
-          </p>
+    <div className="min-h-[88vh] flex bg-cover bg-center">
+      <div className="relative w-1/2 h-screen ">
+        <Image
+          src={LoginBanner}
+          alt="amt"
+          fill
+          className="object-cover"
+        />
+        <div className="absolute inset-0 w-1/2 hidden lg:flex items-center ">
+          <div className="z-10 p-12 text-white h-full">
+            <div className="flex flex-col justify-between h-full">
+              <h1 className="text-[50px]">
+                Học hiệu quả mà thật thoải mái.
+              </h1>
+              <h1 className="text-4xl font-bold">
+                SoundClient
+              </h1>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="w-full lg:w-1/2 flex items-center justify-center p-6">
-        <div className="w-full max-w-md bg-white rounded-2xl shadow-md p-8">
+      <div style={{
+        background: "rgba(255, 255, 255, 0.3)",
+        backdropFilter: "blur(10px)",
+        WebkitBackdropFilter: "blur(10px)",
+      }} className="w-full bg-white lg:w-1/2 flex items-center justify-center p-6 ">
+        <div className="w-full max-w-md rounded-2xl shadow-md p-8 bg-white z-50" >
           <Typography.Title level={3} className="text-center mb-1">
             Welcome Back
           </Typography.Title>
