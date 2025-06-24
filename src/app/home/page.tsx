ae4eaa4fb58054e6961ae8f2953112c10cefe55c
Đào Bá Anh Quân <quandba154@gmail.com>
2025-06-24 20:08:19 +0700

    refactor: clean up imports and improve formatting in HomePage and CommunicationSection components; add ChatBoxAi component with styles

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,38 +1,36 @@
-import Image from "next/image"
-import Banner from "@p/svgs/banner.svg"
+import Image from "next/image";
+import Banner from "@p/svgs/banner.svg";
 // import ProductSection from "@/components/HomePage/productSection/index"
-import BenefitSection from "@/components/HomePage/benefitSection/index"
-import PopularQuestion from "@/components/HomePage/popularQuestion/index"
+import BenefitSection from "@/components/HomePage/benefitSection/index";
+import PopularQuestion from "@/components/HomePage/popularQuestion/index";
+
 
 export default function HomePage() {
-    return (
-        <>
-            <div className="relative w-full h-screen">
-                <Image
-                    src={Banner}
-                    alt="image"
-                    fill
-                    className="object-cover"
-                />
-                <div className="absolute top-10 left-14 text-white flex flex-col items-start gap-4 max-w-xl">
-                    <h1 className="text-[80px] font-bold w-[600px]">
-                        Simple tools for learning and productivity.
-                    </h1>
-                    <p className="text-white text-xl leading-relaxed">
-                        Search millions of study sets or create your own. Improve your grades and productivity with studying tools like flashcards, notes, note-taking templates, practice tests, and study planners.
-                    </p>
-                    <div className="px-8 py-2 bg-[#4255ff] rounded-sm font-medium cursor-pointer">
-                        Get Started
-                    </div>
-                </div>
-            </div>
-            <div className="bg-gray-200">
-                {/* <ProductSection /> */}
-                <BenefitSection />
-                {/* <ProductSection /> */}
-                <PopularQuestion />
-            </div>
+  return (
+    <>
+      <div className="relative w-full h-screen">
+        <Image src={Banner} alt="image" fill className="object-cover" />
+        <div className="absolute top-10 left-14 text-white flex flex-col items-start gap-4 max-w-xl">
+          <h1 className="text-[80px] font-bold w-[600px]">
+            Simple tools for learning and productivity.
+          </h1>
+          <p className="text-white text-xl leading-relaxed">
+            Search millions of study sets or create your own. Improve your
+            grades and productivity with studying tools like flashcards, notes,
+            note-taking templates, practice tests, and study planners.
+          </p>
+          <div className="px-8 py-2 bg-[#4255ff] rounded-sm font-medium cursor-pointer">
+            Get Started
+          </div>
+        </div>
+      </div>
 
-        </>
-    )
+      <div className="bg-gray-200">
+        {/* <ProductSection /> */}
+        <BenefitSection />
+        {/* <ProductSection /> */}
+        <PopularQuestion />
+      </div>
+    </>
+  );
 }
---
+++ src/components/chatbot/index.tsx
@@ -0,0 +1,15 @@
+import Script from "next/script";
+
+export const ChatBoxAi = () => {
+  return (
+    <>
+      <Script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></Script>
+      <df-messenger
+        intent="WELCOME"
+        chat-title="Support"
+        agent-id="0e59920b-32ee-4091-8458-5fabb84f7ce2"
+        language-code="vi"
+      ></df-messenger>
+    </>
+  );
+};
