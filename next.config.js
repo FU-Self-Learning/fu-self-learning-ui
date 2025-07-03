138a6ceb97f369ac62eb76a90fdb8185d21b3716
VoHoangKiet <muoivo.051064@gmail.com>
2025-07-04 01:04:17 +0700

    chore: add alias

--- next.config.js
+++ next.config.js
@@ -10,6 +10,10 @@ const nextConfig = {
       issuer: /\.[jt]sx?$/,
       use: ['@svgr/webpack'],
     });
+
+    // ✅ Thêm alias @
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+
     return config;
   },
 
