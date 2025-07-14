2fc711950f5d73f2c4d85daf9683d959c3a41d49
VoHoangKiet <muoivo.051064@gmail.com>
2025-07-15 02:09:09 +0700

    fix: hard code env

--- src/shared/constants/apiConstants.ts
+++ src/shared/constants/apiConstants.ts
@@ -1,2 +1,2 @@
-export const APP_URL = process.env.NEXT_PUBLIC_API_URL;
+export const APP_URL = 'https://fu-self-learning-api-22235821035.asia-southeast1.run.app/api/v1';
 export const EXPIRED_TOKEN = 100210;
